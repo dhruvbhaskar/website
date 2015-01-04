7d6c798963c11d2bff6c9d4a53f47964968d585e
dhruv <dhruvbhaskar95@gmail.com>
2015-01-04 06:26:55 +0530

    Updates resume and notes

--- notes.php
+++ notes.php
@@ -35,10 +35,6 @@ a:hover{
 }
 </style>
 
-</head>
-
-<body>
-
 <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
 
 <script type="text/x-mathjax-config">
@@ -49,13 +45,46 @@ MathJax.Hub.Config({
 
 </script>
 
+</head>
+
+<body>
+
+<img src="notes/Radio_JOVE.png"/>
+
+<p class="date">
+	November 2015
+</p>
+
+<div class="content" style = "text-align:justify">
+	
+	Radio JOVE is a project initiated by NASA which encourages amateur scientists to observe and analyse natural radio emissions of Jupiter, the Sun and our galaxy. A kit provided by Radio JOVE Project Inc. is used to obtain emission patterns which can then be analysed using softwares like Radio-Skypipe, Radio-Jupiter Pro etc. <br><br>
+	
+	As a team at the University of Calgary, we looked into areas of potential optimization in this project. I attempted to model several designs of the receiver antenna with an aim to reduce the overall size while maintaining performance. Using the Numerical Electromagnetics Code - 2 (<a href = "http://www.qsl.net/4nec2/">4nec2</a>) software, I simulated the electromagnetic response of various antennas at a frequency of 20.1 MHz (corresponding to a wavelength of $\lambda$ = 14.9 meters) which is the frequency of operation of the Radio JOVE set up. Promising results were obtained for the loop design, as summarized below:<br>
+	
+	<img src="notes/loop_design1.png" width="370" height="300" align="right"/><br/>
+	
+	A loop antenna is made up of two conducting loops of wire: the outer loop and the inner coupling loop as shown in the diagram. For optimized results, <br><br> 
+	<center>diameter of outer loop = $\lambda \over 15$ <br><br>
+	diameter of inner coupling loop = <br/>($1 \over 5$) $\times$ (diameter of outer loop) </center><br/><br/><br/>
+	
+	The tuned value of the capacitor was found to be 16.13 pF giving V.S.W.R. = 2.6 and a total maximum gain of 2.53 dBi. The 3D emission pattern is shown below: <br><br>
+	
+	<img src="notes/dipole_fieldpattern.png" width = "320" height = "295"/> 
+	<img src="notes/dipole3D.png" width = "370" height = "400"/><br/><br/><br/>
+	
+	A frequency sweep shows that this design is indeed optimized for our frequency of operation (20.1 MHz): <br/><br/><br/>
+	<center><img src="notes/frequency_sweep.png" width="700" height="500" border="1"/></center><br/><br/>
+	
+</div>
+	
+
 <img src="notes/montecarlo.png"/>
 
 <p class="date">
-	31 December, 2013
+	December 2013
 </p>
 
-<div class="content"><br>
+<div class="content" style = "text-align:justify"><br>
 
 	<img src="notes/image.png" width="200" height="200" align="right"/>
 	
@@ -70,7 +99,7 @@ MathJax.Hub.Config({
 	
 	Figure: Estimates of $\pi$ and corresponding errors vs. number of iterations (generated with GNU Plot)<br><br>
 	
-	<a href="">Source Code</a> in C++ and Bash
+	Code written in C++ and Bash
 
 </div>
 
